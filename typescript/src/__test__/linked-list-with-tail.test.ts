5a615bcf00e42cb37961a87aa9d2994907c21bda
vladimirsan <vladimirsan@gmail.com>
2018-12-20 23:06:26 -0800

    adding tslint and AirBnb

--- typescript/src/__test__/linked-list-with-tail.test.ts
+++ typescript/src/__test__/linked-list-with-tail.test.ts
@@ -1,241 +1,239 @@
-import LinkedList from '../linked-list-with-tail';
-
+import LinkedListWithTail from '../Linked-list-with-tail';
 
 describe('#Linked List', () => {
-    //TODO : Test how the deletion methods change the size
-    describe('#Length', () => {
-        test('A new linked list should have 0 for size', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            expect(list.Length).toEqual(0);
-        });
-
-        test('Appending elements to the link list should increase its size', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            expect(list.Length).toEqual(0);
-            list.AddToHead(1);
-            expect(list.Length).toEqual(1);
-            list.AddToHead(1);
-            expect(list.Length).toEqual(2);
-            list.AddToHead(1);
-            expect(list.Length).toEqual(3);
-        });
-
-        test('Prepending elements to the link list should increase its size', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            expect(list.Length).toEqual(0);
-            list.AddToTail(1);
-            expect(list.Length).toEqual(1);
-            list.AddToTail(1);
-            expect(list.Length).toEqual(2);
-            list.AddToTail(1);
-            expect(list.Length).toEqual(3);
-        });
-        test('Deleting from head should decrease the size', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(1);
-            list.AddToTail(1);
-
-            list.DeleteFromHead();
-            expect(list.Length).toEqual(2);
-            list.DeleteFromHead();
-            expect(list.Length).toEqual(1);
-            list.DeleteFromHead();
-            expect(list.Length).toEqual(0);
-        });
-
-        test('Deleting from tail should decrease the size', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(1);
-            list.AddToTail(1);
-
-            list.DeleteFromTail();
-            expect(list.Length).toEqual(2);
-            list.DeleteFromTail();
-            expect(list.Length).toEqual(1);
-            list.DeleteFromTail();
-            expect(list.Length).toEqual(0);
-        });
+  describe('#Length', () => {
+    test('A new linked list should have 0 for size', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      expect(list.Length).toEqual(0);
     });
 
-    describe('#AddToHead', () => {
-        test('Appending to an empty linked list should result in head and tail being the same', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToHead(1);
-            expect(list.Head).toEqual(list.Tail);
-        });
-
-        test('Testing head/tail separation when adding two nodes', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToHead(2);
-            list.AddToHead(1);
-            expect(list.Head.Value).toEqual(1);
-            expect(list.Tail.Value).toEqual(2);
-            expect(list.Head.Next).toEqual(list.Tail);
-        });
-
-        test('Testing multiple addition', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToHead(3);
-            list.AddToHead(2);
-            list.AddToHead(1);
-            expect(list.Head.Value).toEqual(1);
-            expect(list.Head.Next.Value).toEqual(2);
-            expect(list.Tail.Value).toEqual(3);
-
-            expect(list.Head.Next.Next).toEqual(list.Tail);
-        });
+    test('Appending elements to the link list should increase its size', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      expect(list.Length).toEqual(0);
+      list.addToHead(1);
+      expect(list.Length).toEqual(1);
+      list.addToHead(1);
+      expect(list.Length).toEqual(2);
+      list.addToHead(1);
+      expect(list.Length).toEqual(3);
     });
 
-    describe('#AddToTail', () => {
-        test('AddToTail on an empty linked list should result in head and tail being the same', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            expect(list.Head).toEqual(list.Tail);
-        });
+    test('Prepending elements to the link list should increase its size', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      expect(list.Length).toEqual(0);
+      list.addToTail(1);
+      expect(list.Length).toEqual(1);
+      list.addToTail(1);
+      expect(list.Length).toEqual(2);
+      list.addToTail(1);
+      expect(list.Length).toEqual(3);
+    });
+    test('Deleting from head should decrease the size', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(1);
+      list.addToTail(1);
 
-        test('Testing head/tail separation when adding two nodes', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(2);
-            expect(list.Tail.Value).toEqual(2);
-            expect(list.Head.Value).toEqual(1);
-            expect(list.Head.Next).toEqual(list.Tail);
-        });
-
-        test('Testing multiple addition', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(2);
-            list.AddToTail(3);
-            expect(list.Head.Value).toEqual(1);
-            expect(list.Head.Next.Value).toEqual(2);
-            expect(list.Tail.Value).toEqual(3);
-
-            expect(list.Head.Next.Next).toEqual(list.Tail);
-        });
+      list.deleteFromHead();
+      expect(list.Length).toEqual(2);
+      list.deleteFromHead();
+      expect(list.Length).toEqual(1);
+      list.deleteFromHead();
+      expect(list.Length).toEqual(0);
     });
 
-    describe('#DeleteFromHead', () => {
-        test('Trying to delete from an empty linked list should throw an exception', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            expect(() => list.DeleteFromHead()).toThrow();
-        });
-        test('The function should return the value that has been removed', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(2);
-            list.AddToTail(3);
+    test('Deleting from tail should decrease the size', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(1);
+      list.addToTail(1);
 
-            let deletedValue = list.DeleteFromHead();
-            expect(deletedValue).toEqual(1);
+      list.deleteFromTail();
+      expect(list.Length).toEqual(2);
+      list.deleteFromTail();
+      expect(list.Length).toEqual(1);
+      list.deleteFromTail();
+      expect(list.Length).toEqual(0);
+    });
+  });
 
-            deletedValue = list.DeleteFromHead();
-            expect(deletedValue).toEqual(2);
-
-            deletedValue = list.DeleteFromHead();
-            expect(deletedValue).toEqual(3);
-        });
-        test('Testing Head update', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(2);
-            list.AddToTail(3);
-
-            list.DeleteFromHead();
-            expect(list.Head.Value).toEqual(2);
-
-            list.DeleteFromHead();
-            expect(list.Head.Value).toEqual(3);
-        });
-        test('Testing Head/Tail update while removing all the nodes', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(2);
-            list.AddToTail(3);
-
-            list.DeleteFromHead();
-            list.DeleteFromHead();
-            expect(list.Head).toEqual(list.Tail);
-            list.DeleteFromHead();
-            expect(list.Head).toEqual(null);
-            expect(list.Tail).toEqual(null);
-            expect(() => list.DeleteFromHead()).toThrow();
-        });
+  describe('#addToHead', () => {
+    test('Appending to an empty linked list should result in head and tail being the same', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToHead(1);
+      expect(list.Head).toEqual(list.Tail);
     });
 
-    describe('#DeleteFromTail', () => {
-        test('Trying to delete from an empty linked list should throw an exception', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            expect(() => list.DeleteFromTail()).toThrow();
-        });
-        test('The function should return the value that has been removed', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToHead(1);
-            list.AddToHead(2);
-            list.AddToHead(3);
-
-            let deletedValue = list.DeleteFromTail();
-            expect(deletedValue).toEqual(1);
-
-            deletedValue = list.DeleteFromTail();
-            expect(deletedValue).toEqual(2);
-
-            deletedValue = list.DeleteFromTail();
-            expect(deletedValue).toEqual(3);
-        });
-        test('Testing Tail update', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToHead(1);
-            list.AddToHead(2);
-            list.AddToHead(3);
-
-            list.DeleteFromTail();
-            expect(list.Tail.Value).toEqual(2);
-
-            list.DeleteFromTail();
-            expect(list.Tail.Value).toEqual(3);
-        });
-        test('Testing Tail/Head update while removing all the nodes', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToHead(1);
-            list.AddToHead(2);
-            list.AddToHead(3);
-
-            list.DeleteFromTail();
-            list.DeleteFromTail();
-            expect(list.Head).toEqual(list.Tail);
-            list.DeleteFromTail();
-            expect(list.Head).toEqual(null);
-            expect(list.Tail).toEqual(null);
-            expect(() => list.DeleteFromTail()).toThrow();
-        });
-    });
-    describe('#Iterator', () => {
-        test('An empty linked list should return a null iterator', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            for(let element of list) {
-                // Vinicio - This line should never happen
-                expect(1).toEqual(2);
-            };
-        });
-
-        test('Testing basic for...of', () => {
-            const list:LinkedList<number> = new LinkedList<number>();
-            list.AddToTail(1);
-            list.AddToTail(2);
-            list.AddToTail(3);
-
-            let i = 1;
-            for(let element of list) {
-                expect(element).toEqual(i);
-                i++;
-            }
-        });
+    test('Testing head/tail separation when adding two nodes', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToHead(2);
+      list.addToHead(1);
+      expect(list.Head.value).toEqual(1);
+      expect(list.Tail.value).toEqual(2);
+      expect(list.Head.next).toEqual(list.Tail);
     });
 
-    // describe('', () => {
-        // test('', () => {});
-    // });
+    test('Testing multiple addition', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToHead(3);
+      list.addToHead(2);
+      list.addToHead(1);
+      expect(list.Head.value).toEqual(1);
+      expect(list.Head.next.value).toEqual(2);
+      expect(list.Tail.value).toEqual(3);
+
+      expect(list.Head.next.next).toEqual(list.Tail);
+    });
+  });
+
+  describe('#addToTail', () => {
+    test('addToTail on an empty linked list should result in head and tail being the same', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      expect(list.Head).toEqual(list.Tail);
+    });
+
+    test('Testing head/tail separation when adding two nodes', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(2);
+      expect(list.Tail.value).toEqual(2);
+      expect(list.Head.value).toEqual(1);
+      expect(list.Head.next).toEqual(list.Tail);
+    });
+
+    test('Testing multiple addition', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(2);
+      list.addToTail(3);
+      expect(list.Head.value).toEqual(1);
+      expect(list.Head.next.value).toEqual(2);
+      expect(list.Tail.value).toEqual(3);
+
+      expect(list.Head.next.next).toEqual(list.Tail);
+    });
+  });
+
+  describe('#deleteFromHead', () => {
+    test('Trying to delete from an empty linked list should throw an exception', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      expect(() => list.deleteFromHead()).toThrow();
+    });
+    test('The function should return the value that has been removed', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(2);
+      list.addToTail(3);
+
+      let deletedValue = list.deleteFromHead();
+      expect(deletedValue).toEqual(1);
+
+      deletedValue = list.deleteFromHead();
+      expect(deletedValue).toEqual(2);
+
+      deletedValue = list.deleteFromHead();
+      expect(deletedValue).toEqual(3);
+    });
+    test('Testing Head update', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(2);
+      list.addToTail(3);
+
+      list.deleteFromHead();
+      expect(list.Head.value).toEqual(2);
+
+      list.deleteFromHead();
+      expect(list.Head.value).toEqual(3);
+    });
+    test('Testing Head/Tail update while removing all the nodes', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(2);
+      list.addToTail(3);
+
+      list.deleteFromHead();
+      list.deleteFromHead();
+      expect(list.Head).toEqual(list.Tail);
+      list.deleteFromHead();
+      expect(list.Head).toEqual(null);
+      expect(list.Tail).toEqual(null);
+      expect(() => list.deleteFromHead()).toThrow();
+    });
+  });
+
+  describe('#deleteFromTail', () => {
+    test('Trying to delete from an empty linked list should throw an exception', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      expect(() => list.deleteFromTail()).toThrow();
+    });
+    test('The function should return the value that has been removed', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToHead(1);
+      list.addToHead(2);
+      list.addToHead(3);
+
+      let deletedValue = list.deleteFromTail();
+      expect(deletedValue).toEqual(1);
+
+      deletedValue = list.deleteFromTail();
+      expect(deletedValue).toEqual(2);
+
+      deletedValue = list.deleteFromTail();
+      expect(deletedValue).toEqual(3);
+    });
+    test('Testing Tail update', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToHead(1);
+      list.addToHead(2);
+      list.addToHead(3);
+
+      list.deleteFromTail();
+      expect(list.Tail.value).toEqual(2);
+
+      list.deleteFromTail();
+      expect(list.Tail.value).toEqual(3);
+    });
+    test('Testing Tail/Head update while removing all the nodes', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToHead(1);
+      list.addToHead(2);
+      list.addToHead(3);
+
+      list.deleteFromTail();
+      list.deleteFromTail();
+      expect(list.Head).toEqual(list.Tail);
+      list.deleteFromTail();
+      expect(list.Head).toEqual(null);
+      expect(list.Tail).toEqual(null);
+      expect(() => list.deleteFromTail()).toThrow();
+    });
+  });
+  describe('#Iterator', () => {
+    test('An empty linked list should return a null iterator', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      for (const element of list) {
+        // Vinicio - This line should never happen
+        expect(1).toEqual(2);
+      }
+    });
+
+    test('Testing basic for...of', () => {
+      const list: LinkedListWithTail<number> = new LinkedListWithTail<number>();
+      list.addToTail(1);
+      list.addToTail(2);
+      list.addToTail(3);
+
+      let i = 1;
+      for (const element of list) {
+        expect(element).toEqual(i);
+        i += 1;
+      }
+    });
+  });
+
+  // describe('', () => {
+  // test('', () => {});
+  // });
 });
